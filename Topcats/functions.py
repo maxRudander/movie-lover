bb41795fc2a5c74e942475cf6db6633f111fe6a7
Christopher Lindberg <christopher.lindberg1@gmail.com>
2019-01-04 11:29:43 +0100

    Revert "Removing dependencies that were not needed"
    This reverts commit 62c6883769fdfbed474fa30767d413251258a10c.

---
+++ Topcats/functions.py
@@ -0,0 +1,27 @@
+from os import listdir
+
+#Author: Martin
+def get_headlines():
+    '''Listar namnen på alla text-filer i mappen cleaning_articles, samt tar bort filändelsen.'''
+    headlines = sorted(listdir("static/cleaning_articles"))
+    headline_list = []
+
+    for headline in headlines:
+        headline = headline[:-4]
+        headline_list.append(headline)
+    return headline_list
+
+#Author: Martin
+def get_title_content(a):
+    '''Skapar utifrån textfilerna en väg, en läs-variabel och en variabel för text-filens innehåll.'''
+    headlines = listdir("static/cleaning_articles")
+    headline_list = []
+    for headline in headlines:
+        if headline == a + ".txt":
+            path = "static/cleaning_articles/" + headline
+            fill = days_file = open(path,'r')
+            text = fill.read()
+            fill.close()
+        else:
+            print("test")
+    return (text)
