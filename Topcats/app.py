c1438e191ad259c40b6802c2832d9c34c0b9e6e9
Christopher Lindberg <christopher.lindberg1@gmail.com>
2019-01-10 13:50:17 +0100

    minor changes

--- Topcats/app.py
+++ Topcats/app.py
@@ -3,14 +3,14 @@ from functions import get_headlines, get_title_content
 
 app = Flask(__name__)
 
-""" 
+"""
 API data-requests go to: http://www.omdbapi.com/?apikey=[db0723c0]&
 API poster-requests go to: http://img.omdbapi.com/?apikey=[db0723c0]&
 """
 
 @app.route("/")
 def index():
-    return render_template("index.html", author="Christopher")
+    return render_template("index.html")
 
 '''@app.route("/static/<path:path>")
 def serve_static_files(path):
