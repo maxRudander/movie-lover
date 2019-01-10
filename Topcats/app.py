4eff0e829a35a8b0260b45b0368ff2d3834b0e42
Tobias <tobiashakanthoren@gmail.com>
2019-01-10 14:34:56 +0100

    basic search functionality in url

--- Topcats/app.py
+++ Topcats/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from functions import get_headlines, get_title_content
+import omdb
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/<film>")
+def movie_request(film):
+	film_data = {
+			'title': omdb.get_movie(film)['Title'],
+			'plot': omdb.get_movie(film)['Plot']
+			}
+	return render_template("test.html", film_data=film_data)
 
 '''@app.route("/static/<path:path>")
 def serve_static_files(path):
--- Topcats/omdb.py
+++ Topcats/omdb.py
@@ -1,9 +1,10 @@
 import requests
 
 
-data_response = requests.get('http://www.omdbapi.com/?t=taken&apikey=f6aaec81')
-if data_response.status_code is not 200:
-	# Bad response / something went wrong
-	print(data_response.status_code)
-else:
-	print(data_response.json()['Plot'])
+def get_movie(film):
+	data_response = requests.get(f'http://www.omdbapi.com/?t={film}&apikey=f6aaec81')
+	if data_response.status_code is not 200:
+		# Bad response / something went wrong
+		print(data_response.status_code)
+	else:
+		return data_response.json()
