49b77a76d6b31d7a2322312438af6c00faba3ba3
Tobias <tobiashakanthoren@gmail.com>
2019-01-10 14:18:24 +0100

    initial omdb.py file

--- Topcats/omdb.py
+++ Topcats/omdb.py
@@ -1,14 +1,9 @@
 import requests
-import omdb
 
-""" 
-API data-requests go to: http://www.omdbapi.com/?apikey=[db0723c0]&
-API poster-requests go to: http://img.omdbapi.com/?apikey=[db0723c0]&
-"""
 
 data_response = requests.get('http://www.omdbapi.com/?t=taken&apikey=f6aaec81')
 if data_response.status_code is not 200:
 	# Bad response / something went wrong
 	print(data_response.status_code)
-for item in data_response.json():
-	print(item)
+else:
+	print(data_response.json()['Plot'])
