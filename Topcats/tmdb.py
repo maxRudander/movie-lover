d3082278648fb74d7f349f8cb8c2d6b603da8bfa
Christopher Lindberg <christopher.lindberg1@gmail.com>
2019-04-04 18:10:51 +0200

    Korrigerade try-except för IndexError

--- Topcats/tmdb.py
+++ Topcats/tmdb.py
@@ -6,11 +6,10 @@ def get_trailer(film):
 	Letar efter en trailer från tmdb och returnerar denna ifall en hittas.
 	"""
 
-	data_response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key=d4e35699aa07152cd0a3e572fff5f595&language=en-US&query={film}&page=1&include_adult=false').json()
-	id = data_response['results'][0]['id']
-	video_response = requests.get(f'http://api.themoviedb.org/3/movie/{id}/videos?api_key=d4e35699aa07152cd0a3e572fff5f595').json()
-
 	try:
+		data_response = requests.get(f'https://api.themoviedb.org/3/search/movie?api_key=d4e35699aa07152cd0a3e572fff5f595&language=en-US&query={film}&page=1&include_adult=false').json()
+		id = data_response['results'][0]['id']
+		video_response = requests.get(f'http://api.themoviedb.org/3/movie/{id}/videos?api_key=d4e35699aa07152cd0a3e572fff5f595').json()
 		return 'https://www.youtube.com/embed/' + video_response['results'][0]['key']
 	except IndexError:
    		return None
